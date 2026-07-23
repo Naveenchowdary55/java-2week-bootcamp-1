d043b127937e5e9e55d52c1c9ba0c6fb5b5a324a
Naveen <naveen.chowdary5568@gmail.com>
2026-07-23 12:50:19 +0530

    shape.java

--- src/main/java/day09/Shape.java
+++ src/main/java/day09/Shape.java
@@ -13,6 +13,9 @@ package day09;
  * area" without pretending to compute one.
  */
 public class Shape {
+    public Shape() {
+        // nothing to do
+    }
 
     /**
      * @return the area of this shape. The base version returns {@code 0.0};
@@ -20,5 +23,65 @@ public class Shape {
      */
     public double area() {
         return 0.0;
+        
+    }
+}
+class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+}
+
+class Triangle extends Shape {
+
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Shape s1 = new Circle(2);
+        Shape s2 = new Rectangle(4, 5);
+        Shape s3 = new Triangle(6, 4);
+
+        System.out.println("Circle Area    : " + s1.area());
+        System.out.println("Rectangle Area : " + s2.area());
+        System.out.println("Triangle Area  : " + s3.area());
     }
 }
